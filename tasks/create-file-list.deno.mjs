785fb07b744d5804a620f5009863b9a893daaabc
Edditoria <edditoria@pm.me>
2022-10-12 15:40:33 +0800

    Update deno task create-list

--- tasks/create-file-list.deno.mjs
+++ tasks/create-file-list.deno.mjs
@@ -1,7 +1,15 @@
 import inspectedData from '../reports/inspected-assets-list.json' assert { type: 'json' };
 
-const outputData = inspectedData.passed;
+const fileList = [];
+for (const eachRelease of inspectedData.passed) {
+	const files = { tag: eachRelease.tag };
+	for (const key in eachRelease.files) {
+		files[key] = eachRelease.files[key];
+	}
+	fileList.push(files);
+}
+
 const outputFile = 'reports/file-list.json';
 await Deno.mkdir('reports', { recursive: true });
-await Deno.writeTextFile(outputFile, JSON.stringify(outputData, null, '\t'));
+await Deno.writeTextFile(outputFile, JSON.stringify(fileList, null, '\t'));
 console.log(`File written to ${outputFile}`);
