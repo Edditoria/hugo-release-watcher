773466c82bdcb32970f81bf46ab6cc2f7e071613
Edditoria <edditoria@pm.me>
2022-11-11 15:17:42 +0800

    Add deno task print-keys

---
+++ tasks/print-keys.deno.mjs
@@ -0,0 +1,18 @@
+import inspectedData from '../reports/inspected-assets-list.json' assert { type: 'json' };
+
+const fileList = inspectedData.passed;
+const keys = [];
+for (const eachRelease of fileList) {
+	for (const eachKey in eachRelease.files) {
+		if (eachKey === 'tag') {
+			continue;
+		}
+		if (!keys.includes(eachKey)) {
+			keys.push(eachKey);
+		}
+	}
+}
+
+keys.sort();
+console.log(`Keys counted: ${keys.length}`);
+keys.forEach((key) => console.log(key));
